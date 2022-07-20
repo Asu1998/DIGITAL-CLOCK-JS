6f49faaa6f867ef9386f4d308385885e3aa8f603
Asutosh Nayak <nayak.asutosh10@gmail.com>
2022-07-20 11:48:26 +0530

    Changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function clock(){
     var min = hms.getMinutes();
     var sec = hms.getSeconds();
     var apm = document.getElementById("apm");
+    var hours = hms.getHours();
 
     if(hour >= 12){
         apm.innerText = 'PM';
@@ -14,66 +15,96 @@ function clock(){
     if(hour > 12){
         hour = hour - 12;
     }
-    if(hour < 10){
-        "0" + hms.getHours();
+    if((hour+"").length === 1){
+        hour = "0"+hour;
     }
-    if(min < 10){
-        "0" + hms.getMinutes();
+    if((min+"").length === 1){
+        min = "0"+min;
     }
-    if(sec < 10){
-        "0" + hms.getSeconds;
+    if((sec+"").length === 1){
+        sec = "0"+sec;
     }
-    // const elementOne = document.createElement("img");
 
+    if(dropOne == hours){
+        document.getElementsByClassName("p2").innerText = "GOOD MORNING !! WAKE UP !!";
+        document.getElementsByClassName("rp4").innerText = "GRAB SOME HEALTHY BREAKFAST !!!";
+        document.getElementsByClassName("rdv5")[0].style.backgroundImage = "url('mrng.png')";
+        
+    }
+    else if(dropTwo == hours){
+        document.getElementsByClassName("p2").innerText = "GOOD AFTERNOON !! TAKE SOME SLEEP !!";
+        document.getElementsByClassName("rp4").innerText = "LET'S HAVE SOME LUNCH !!!";
+        document.getElementsByClassName("rdv5")[0].style.backgroundImage = "url('lunch.png')";
+        
+    }
+    else if(dropThree == hours){
+        document.getElementsByClassName("p2").innerText = "GOOD EVENING !!";
+        document.getElementsByClassName("rp4").innerText = "STOP YAWNING, GET SOME TEA.. IT'S JUST EVENING !!!";
+        document.getElementsByClassName("rdv5")[0].style.backgroundImage = "url('tea.png')";
+        
+    }
+    else if(dropFour == hours){
+        document.getElementsByClassName("p2").innerText = "GOOD NIGHT !!";
+        document.getElementsByClassName("rp4").innerText = "CLOSE YOUR EYES AND GO TO SLEEP !!!";
+        document.getElementsByClassName("rdv5")[0].style.backgroundImage = "url('sleep.png')";
+        
+    }
+    else{
+        document.getElementsByClassName("p2").innerText = "GOOD MORNING !! WAKE UP !!";
+        document.getElementsByClassName("rp4").innerText = "GRAB SOME HEALTHY BREAKFAST !!!";
+        document.getElementsByClassName("rdv5")[0].style.backgroundImage = "url('mrng.png')";
+        
+    }
 
     document.getElementById("hour").innerHTML = hour + `<br>hour`;
     document.getElementById("min").innerHTML = min + `<br>min`;
     document.getElementById("sec").innerHTML = sec + `<br>sec`;
 
-    setTimeout(clock,1000);
+    
 };
-clock();
+setInterval(clock,1000);
 
-function clockOne(){
-    var date = new Date();
-    var hourOne = date.getHours();
+var morning = document.getElementById("up");
+var afternoon = document.getElementById("lunch");
+var code = document.getElementById("play");
+var night = document.getElementById("sleep");
 
-    let hr = hourOne;
-    let pTwo = document.getElementsByClassName("p2");
-    let pFour = document.getElementsByClassName("rp4");
-    let dFive = document.getElementsByClassName("rdv5");
+var dropOne = morning.options[morning.selectedIndex].text;
+var dropTwo = afternoon.options[afternoon.selectedIndex].text;
+var dropThree = code.options[code.selectedIndex].text;
+var dropFour = night.options[night.selectedIndex].text;
 
-    if((hr > 7 && hr < 12)){
-        pTwo[0].innerText = "GRAB SOME HEALTHY BREAKFAST !!!";
-        pFour[0].innerText = "GOOD MORNING !! WAKE UP !!";
-        dFive.style.backgroundImage ="url('mrng.png')";
-    }
-    else if((hr > 12 && hr < 16)){
-        pTwo[0].innerText = "LET'S HAVE SOME LUNCH !!!";
-        pFour[0].innerText = "GOOD AFTERNOON !! TAKE SOME SLEEP";
-        dFive.style.backgroundImage ="url('lunch.png')";
-    }
-    else if((hr > 16 && hr < 20)){
-        pTwo[0].innerText = "GOOD EVENING !!!";
-        pFour[0].innerText = "STOP YAWNING, GET SOME TEA.. IT'S JUST EVENING";
-        dFive.style.backgroundImage ="url('tea.png')";
-    }
-    else if((hr > 20 && hr < 26)){
-        pTwo[0].innerText = "GOOD NIGHT !!!";
-        pFour[0].innerText = "CLOSE YOUR EYES AND GO TO SLEEP";
-        dFive.style.backgroundImage ="url('sleep.png')";
-    }
-    // setTimeout(clockOne,1000);
-}
-clockOne();
+var playOne = morning.options[morning.selectedIndex].value;
+var playTwo = afternoon.options[afternoon.selectedIndex].value;
+var playThree = code.options[code.selectedIndex].value;
+var playFour = night.options[night.selectedIndex].value;
 
-let button = document.getElementById('lbt1');
+function realPlay(){
+    dropOne = morning.options[morning.selectedIndex].text;
+    dropTwo = afternoon.options[afternoon.selectedIndex].text;
+    dropThree = code.options[code.selectedIndex].text;
+    dropFour = night.options[night.selectedIndex].text;
+
+    playOne = morning.options[morning.selectedIndex].value;
+    playTwo = afternoon.options[afternoon.selectedIndex].value;
+    playThree = code.options[code.selectedIndex].value;
+    playFour = night.options[night.selectedIndex].value;
+    
+    document.getElementById("wt").innerText = dropOne;
+    document.getElementById("lt").innerText = dropTwo;
+    document.getElementById("nt").innerText = dropThree;
+    document.getElementById("ngt").innerText = dropFour;
+};
+realPlay();
+
+let button = document.getElementById("lbt1");
+
 let bfrHover = () =>{
-    button.innerHTML = "Party Time !!";
+    button[0].innerHTML = "Party Time !!";
     button.style.backgroundImage = "linear-gradient(to right, #6E54EC 0%, #CB52F8 25%, #FC5EFF 75%, #F1A7C5 100%)";
 }
 
 let aftHover = () => {
-    button.innerHTML = "Set Alarm";
+    button[0].innerHTML = "Set Alarm";
     button.style.backgroundImage = "linear-gradient(to right, #6E54EC 0%, #CB52F8 25%, #FC5EFF 75%, #F1A7C5 100%)";
 }
